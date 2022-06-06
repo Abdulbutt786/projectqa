aa9e92bf47a690e15187f7b17f9a287c06889b60
Abdul <Abdulbutt123@gmail.com>
2022-06-06 21:57:27 +0100

    ab:added forms and a few bugs need fixing

--- application/models.py
+++ application/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     first_name = db.Column(db.String(40), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
-    user_email = db.Column(db.String(100), nullable=False)
+    email_address = db.Column(db.String(100), nullable=False)
     contact_number = db.Column(db.Integer,nullable=False, default="0712345678")
     reveiws = db.relationship("Review", backref="reviewbr")
 
@@ -16,4 +16,4 @@ class Review(db.Model):
     review_rating = db.Column(db.String(2), nullable = False)
     review_description = db.Column(db.String(500), nullable = False)
     review_date = db.Column(db.DateTime, nullable = False, default = datetime.now)
-    User = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False)
--- application/routes.py
+++ application/routes.py
@@ -1,3 +1,4 @@
+from pdb import post_mortem
 from application import app, db
 from application.forms import UserForm
 from application.forms import ReviewForm
@@ -7,4 +8,34 @@ from flask import render_template, request, redirect, url_for
 @app.route('/')
 def index():
     all_users = User.query.all()
-    return render_template('/index.html',all_users = all_users)
+    return render_template('index.html', all_users = all_users)
+
+
+@app.route("/allmovierev/<int:user_id>")
+def allreviews(user_id):
+    submitted_reviews=Review.query.filterby(user_id = user_id).all
+    return render_template("usermovierev.html", submitted_reviews= submitted_reviews)
+
+@app.route("/new_user", methods = ["GET", "POST"])
+def new_user():
+    form = UserForm()
+    if request.method == "POST":
+        user_details = User(first_name = form.first_name.data, last_name = form.last_name.data, email_address = form.email_address.data, contact_number = form.phone_number.data)
+        db.session.add(user_details)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("newuser.html", form = form)
+
+@app.route("/create_review/<user_id>")
+def create_review(user_id):
+    form= ReviewForm()
+    if request.method == "POST":
+        review = Review(movie_name= form.movie_name.data, genre_movie=form.genre_movie.data, review_description = form.review_description.data,date_watched_movie = form.date_watched_movie.data)
+        db.session.add(review)
+        db.session.commit()
+    return render_template("create_review.html")
+
+
+
+
+
