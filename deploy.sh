2576146d22bb750b32a92ba19b0d6302d4324dde
Abdul <Abdulbutt123@gmail.com>
2022-06-17 14:49:10 +0100

    debugging deployment

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 echo "killing existing processes"
-# sudo kill -9 $(sudo lsof -i :5000 | cut -d' ' -f2 | tail -n 5 | head -n 1)
+sudo kill -9 $(sudo lsof -i :5000 | cut -d' ' -f2 | tail -n 5 | head -n 1)
 python3 create.py
 python3 -m gunicorn -D --workers 4 --bind 0.0.0.0:5000 app:app
